5641ca6221c3e5942044677bd221ac0b00f2371a
NimeshNayaju <nimeshnayaju@gmail.com>
2019-01-19 12:53:09 -0400

    Added server.js

---
+++ server/server.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//  create a Todo model
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Goto gym at 5'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+
+var otherTodo = new Todo({
+  text: 'Eat breakfast',
+  completed: true,
+  completedAt: 123
+});
+
+otherTodo.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+  console.log('Unable to save todo');
+});
+
+
+
+
+
