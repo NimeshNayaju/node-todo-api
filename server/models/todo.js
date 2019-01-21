aa1b9ef17db817cc3e18b598c49df3b23b8fd747
NimeshNayaju <nimeshnayaju@gmail.com>
2019-01-21 11:14:41 -0400

    Added POST /todos route and refactored mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose')
+
+//  create a Todo model
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Goto gym at 5'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+
+// var otherTodo = new Todo({
+//   text: 'Gym at 5'
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+module.exports = {
+  Todo
+};
+
