2e01e1728b4d05263a47ce9c2dcef24ad6437640
NimeshNayaju <nimeshnayaju@gmail.com>
2019-02-09 16:34:18 -0400

    updated for heroku

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user.js');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
