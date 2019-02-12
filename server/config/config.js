5a6e7055a430f0dcdd9c4ec403cccb5bdbfc2d9e
NimeshNayaju <nimeshnayaju@gmail.com>
2019-02-12 01:05:04 -0400

    Updated config to use config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,22 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json')
+  var envConfig = config[env];
+
+  // Object.keys takes an object, gets all the keys and returns them as array 
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+
+
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
